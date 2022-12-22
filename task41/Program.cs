e242f2f1c285741fdfe7a3bf589a5c2fa8871e5e
s.kosova <s.kosova@mail.com>
2022-12-23 00:12:02 +0300

    23122022

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Input(string welcome)
+{
+    Console.Write(welcome);
+    return Convert.ToInt32(Console.ReadLine());
+}
+// что такое M... тогда с количеством можно определиться по ходу ввода. 
+// нет в ТЗ - переложи выбор на юзера)
+int count = 0;
+int number = Input("вводите числа, пока не надоест. чтобы остановить безумие, введите '4242': ");
+if (number!=4242) do 
+{
+    if (number > 0) count = count + 1;
+    number = Input("еще число? ");
+}
+while (number != 4242);
+Console.WriteLine($"чисел больше нуля: {count+1}");
---
+++ task43/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+int Input(string welcome)
+{
+    Console.Write(welcome);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void Intersection (int k1, int b1, int k2, int b2)
+{
+if(k1 == k2) Console.WriteLine("параллельные, получается");
+else
+    {
+    int x = (b1-b2)/(k2-k1);
+    int y = (k2*b1-k1*b2)/(k2-k1);
+    Console.WriteLine($"корды: {x}, {y}");
+    }
+}
+int k1 = Input("k1: ");
+int b1 = Input("b1: ");
+int k2 = Input("k2: ");
+int b2 = Input("b2: ");
+Intersection(k1,b1,k2,b2);
