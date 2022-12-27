83e8e9a9e219fe7291c819b360d5f807dc0076e4
s.kosova <s.kosova@mail.com>
2022-12-27 19:56:43 +0300

    2022-12-27

---
+++ task47/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int Input(string welcome)
+{
+    Console.Write(welcome);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = Input("m = ");
+int n = Input("n = ");
+double[,] arr = new double[m,n];
+double Random()
+{
+    Random r = new Random();
+    return r.NextDouble()*10;
+}
+for (int i = 0; i<m; i++)
+{
+    for (int j = 0; j<n; j++)
+    {
+        arr[i,j] = Math.Round(Random(),1);
+        Console.Write(arr[i,j] + "  ");
+    
+    }
+    Console.WriteLine();
+}
---
+++ task52/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int Input(string welcome)
+{
+    Console.Write(welcome);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = Input("m = ");
+int n = Input("n = ");
+int[,] arr = new int[m,n];
+int Random()
+{
+    Random r = new Random();
+    return r.Next(0,42);
+}
+for (int i = 0; i<m; i++)
+{
+    for (int j = 0; j<n; j++)
+    {
+        arr[i,j] = Random(); 
+        Console.Write(arr[i,j] + "  ");
+    }
+Console.WriteLine();
+//avg = sum/n;
+}
+//Console.WriteLine(avg);
+int sum = 0;
+for (int j = 0; j<n; j++)
+{
+    for (int i = 0; i<m; i++)
+    {
+        sum = sum + arr[i,j];
+    }
+Console.Write(sum/m + " ");
+}
