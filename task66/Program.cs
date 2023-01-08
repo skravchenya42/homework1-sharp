3f87d2970fe7096044e818455d02f6f169388393
s.kosova <s.kosova@mail.com>
2023-01-08 23:10:13 +0300

    08 01 2023

---
+++ task66/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// ИНПУТ ДЛЯ ЦЕЛЫХ ЧИСЕЛ
+int Input(string welcome)
+{
+    Console.Write(welcome);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// УРАВНИТЕЛЬ ДВУХ ЧИСЕЛ (ЗАОДНО СЧИТАЕТ СУММУ)
+int SumDiff(int M,int N,int result)
+{
+
+    if (M == N) return result;
+    if (M<N)
+    {
+        M++;
+        result = result+M;
+        return SumDiff(M,N,result);
+    }
+    else
+    {
+        N++;
+        result = result+N;
+        return SumDiff(M,N,result);
+    }
+
+}
+
+int M = Input("натуральное M = ");
+int N = Input("натуральное N = ");
+Console.Write(SumDiff(M,N,0));
+
